7cce4a986db45fb375a5305f95031b17abce4922
Raj-Mane-Developer <wildliferaj28@gmail.com>
2026-07-29 11:42:10 +0530

    java

--- core_java_2/src/core_java_2_practicee/First.java
+++ core_java_2/src/core_java_2_practicee/First.java
@@ -2,27 +2,28 @@ package core_java_2_practicee;
 
 public class First {
 
-	public static void main(String[] args) {
-	
-	Student one=new Student();
-	
-	
-	}
 
-}
-class Student
-{
-	String name ;
-	String Email;
-	
-	public void setName(String nm)
-	{
-		if(nm.length()>=4)
-		{
-			this.name=nm;
+		public static void main(String[] args) {
+		
+		Car car=new Car();
+		
+		car.run(106, "BB", 69);
+		
 		}
+
 	}
+	class Car
+	{
+		String colour;
+		double price;
+		String company;
+		
+		public void run(int i, String s , int j)
+		{
+			System.out.println("car is running");
+			System.out.println(i+j);
+			System.out.println(s.toUpperCase());
+		}
 
 
-
-}
+	}
